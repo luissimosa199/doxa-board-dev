65ae13569faa9bb179cdb3806c0dbae270e6daa9
Luis <simosa37@gmail.com>
2023-09-14 15:13:37 -0300

    feat: user agent logic now persist in db

--- src/pages/api/user_agent_info.ts
+++ src/pages/api/user_agent_info.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "../../db/dbConnect";
 import { UserAgentModel } from "../../db/models";
-import { serialize } from "cookie";
+import { serialize, parse } from "cookie";
 import { v4 as uuidv4 } from "uuid";
+import { UserModel } from "@/db/models/userModel";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,11 +13,26 @@ export default async function handler(
 ) {
   const { userData, id } = req.body;
 
+  const session = await getServerSession(req, res, authOptions);
+
+  if (session && session.user) {
+    const userId = session.user.email;
+    const cookies = parse(req.headers.cookie || "");
+    const userAgentId = cookies.user_agent_id;
+
+    const updatedUser = await UserModel.findOneAndUpdate(
+      { email: userId },
+      { $addToSet: { user_agent_id: userAgentId } },
+      { new: true, upsert: false }
+    );
+  }
+
   if (req.method === "POST") {
     const newId = uuidv4();
     try {
-      dbConnect();
+      await dbConnect();
       const userAgent = new UserAgentModel({ visits: [userData], _id: newId });
+
       await userAgent.save();
     } catch (error) {
       console.error("Error creating UserAgent:", error);
@@ -34,7 +52,7 @@ export default async function handler(
     res.status(200).json({ message: "userAgent registered" });
   } else if (req.method === "PUT") {
     try {
-      dbConnect();
+      await dbConnect();
       await UserAgentModel.findByIdAndUpdate(id, {
         $push: { visits: userData },
       });
