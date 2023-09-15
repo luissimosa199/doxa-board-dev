81545f9264e059eae5eb0288f5216ca63fe6e5cc
Luis <simosa37@gmail.com>
2023-09-15 15:08:53 -0300

    feat: admin view

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -2,6 +2,13 @@ import { getServerSession } from "next-auth"
 import { GetServerSideProps } from "next/types"
 import { FunctionComponent } from "react"
 import { CustomSession, authOptions } from "./api/auth/[...nextauth]"
+import { useQuery } from "@tanstack/react-query"
+import GlobalInfoTable from "@/components/AdminPanelGlobalInfoTable"
+import TenUsersInfoTable from "@/components/AdminPanelTenUsersInfoTable"
+import { fetchUserAgentData } from "@/utils/adminPanelHelpers"
+import UTMAnalyticsTable from "@/components/AdminPanelUTMAnalitycsTable"
+import { UserAgent } from '@/utils/adminPanelHelpers';
+import VisitsTable from "@/components/AdminPanelVisitsTable"
 
 interface AdminPageInterface {
     name: string
@@ -10,13 +17,45 @@ interface AdminPageInterface {
 
 const Admin: FunctionComponent<AdminPageInterface> = ({ name, email }) => {
 
+    const { data, isLoading, error } = useQuery(["user_agent_data"], fetchUserAgentData)
+ 
+    if (isLoading) {
+        return (
+            <p>Cargando datos...</p>
+        )
+    }
+
+    if (error) {
+        return (
+            <p>Error. {JSON.stringify(error)}</p>
+        )
+    }
+
+    const allVisits = data.userAgentData.flatMap((item: UserAgent) => item.visits);
 
     // Your component logic remains here
     return (
         <div>
             <p>Logueado como {name} ({email})</p>
 
+            <div className="mb-12">
+                <GlobalInfoTable data={data.userAgentData} />
+            </div>
+
+            <div className="mb-12">
+                <TenUsersInfoTable data={data.userAgentData} users={data.users} />
+            </div>
+
+            <div className="mb-12">
+                <UTMAnalyticsTable data={data.userAgentData} />
+            </div>
+
+            <div className="mb-12">
+                <VisitsTable visits={allVisits}/>
+            </div>
+
         </div>
+
     )
 }
 
@@ -32,7 +71,6 @@ export const getServerSideProps: GetServerSideProps<AdminPageInterface> = async 
         }
     }
 
-    // If the user is an admin, proceed to render the page
     return {
         props: {
             name: session?.user?.name || "Usuario",
