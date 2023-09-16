e4099291c51d113f94ddd85863f50bc60b97ba29
Luis <simosa37@gmail.com>
2023-09-16 15:03:31 -0300

    feat: change password flow implemented

--- src/pages/cambiarpassword.tsx
+++ src/pages/cambiarpassword.tsx
@@ -6,19 +6,68 @@ export default function ChangePassword() {
 
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [errorMessage, setErrorMessage] = useState<string | null>(null)
+    const [mailValidated, setMailValidated] = useState<boolean>(false)
+    const [codeSended, setCodeSended] = useState<boolean>(false)
 
     const router = useRouter()
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+
         e.preventDefault();
-        setIsLoading(true)
         const formData = new FormData(e.currentTarget);
         const email = formData.get('email');
+
+        if (!mailValidated) {
+
+            if (!codeSended) {
+
+                console.log("mail no validado, codigo no enviado")
+
+                const response = await fetch('/api/cambiarpassword/verify', {
+                    method: 'POST',
+                    body: JSON.stringify({ email }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (response.ok) {
+                    console.log("mail no validado, codigo enviado 1")
+                    setCodeSended(true)
+                } else {
+                    // set error message
+                    console.log("error: 1")
+                }
+
+            } else {
+                const code = formData.get('code');
+
+                console.log("mail no validado, codigo enviado 2")
+                const response = await fetch('/api/cambiarpassword/verify', {
+                    method: 'POST',
+                    body: JSON.stringify({ email, code }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (response.ok) {
+                    console.log("mail validado, codigo enviado")
+                    setMailValidated(true)
+                } else {
+                    // set error message
+                    console.log("error: 2")
+                }
+            }
+            return
+        }
+
+        setIsLoading(true)
         const newPassword = formData.get('newPassword');
 
         const response = await fetch('/api/cambiarpassword', {
             method: 'POST',
-            body: JSON.stringify({ email, newPassword }),
+            body: JSON.stringify({ email, newPassword, mailValidated }),
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -38,23 +87,32 @@ export default function ChangePassword() {
         <div className="min-h-screen flex justify-center items-center bg-gray-100">
             <form className="bg-white p-8 rounded-lg shadow-md w-96" onSubmit={handleSubmit}>
                 <div className="mb-4">
+                    {!codeSended && <p className="mb-2">Primero necesitamos validar tu email</p>}
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                         Email
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" id="email" required />
                 </div>
-                <div className="mb-6">
+                {codeSended && !mailValidated && <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="code">
+                        Introduce el numero que te ha llegado al email
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="code" type="code" id="text" required />
+                </div>}
+
+                {mailValidated && <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="newPassword">
                         Nueva Contraseña
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="newPassword" type="password" id="newPassword" required />
-                </div>
+                </div>}
+
                 {errorMessage && <div className="mb-6">
                     <p className="text-sm text-red-600 font-semibold text-center">Error: {errorMessage}</p>
                 </div>}
                 <div className="flex flex-col items-center justify-between">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-all" type="submit" disabled={isLoading}>
-                        {isLoading ? "Cambiando..." : "Cambiar Contraseña"}
+                        {!mailValidated ? "Verificar email" : isLoading ? "Cambiando..." : "Cambiar Contraseña"}
                     </button>
                     <Link href="/login" className="text-sm mt-2 text-slate-500 hover:opacity-75 transition-all">Volver al inicio de sesión</Link>
                 </div>
