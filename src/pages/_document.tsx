324654d9e5b3e49d591fee9871d5fbe519fb91df
Luis <simosa37@gmail.com>
2023-10-06 08:54:38 -0300

    ads activated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,14 @@
-import Ad from '@/components/Ad';
-import { Html, Head, Main, NextScript } from 'next/document';
+import Ad from "@/components/Ad";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=G-3R9LM83VDP`}></script>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-3R9LM83VDP`}
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -13,18 +16,21 @@ export default function Document() {
                 function gtag(){window.dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-3R9LM83VDP');
-              `
+              `,
           }}
         />
 
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2371684572387469"
-          crossOrigin="anonymous"></script> */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2371684572387469"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
-        {/* <Ad /> */}
+        <Ad />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
